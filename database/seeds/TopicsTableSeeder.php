a2e6480d9a385f1f1eafc508b8c1390e50294e7f
James <296466865@qq.com>
2019-04-24 06:26:08 +0000

    填充用户和话题数据

---
+++ database/seeds/TopicsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+use App\Models\User;
+use App\Models\Category;
+
+class TopicsTableSeeder extends Seeder
+{
+    public function run()
+    {
+         //取出所有用户ID成一个数组
+        $user_ids =User::all()->pluck('id')->toArray();
+
+        //所有分类Id数组，
+        $category_ids =Category::all()->pluck('id')->toArray();
+
+        //获取fake实例
+        $faker =app(Faker\Generator::class);
+
+        $topics = factory(Topic::class)
+                        ->times(100)
+                        ->make()
+                        ->each(function($topic,$index)
+                            use($user_ids,$category_ids,$faker){
+                                //从用户Id数组中随机取出一个并赋值
+                                $topic->user_id =$faker->randomElement($user_ids);
+
+                                //话题分类 同上
+                                $topic->category_id =$faker->randomElement($category_ids);
+                            }
+                    );
+
+        //将数据集合转换为数组  并插入到数据库中
+        Topic::insert($topics->toArray());
+    }
+
+}
+
