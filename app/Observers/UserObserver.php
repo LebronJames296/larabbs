340e87668ff44270b8486930911e576d41eb1b0c
James <296466865@qq.com>
2019-05-03 12:36:15 +0000

    “用户注册默认头像,搞定收工”

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -14,8 +14,11 @@ class UserObserver
         //
     }
 
-    public function updating(User $user)
+    public function saving(User $user)
     {
         //
+        if(empty($user->avatar)){
+            $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
     }
 }
