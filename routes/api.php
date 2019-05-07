52533db6e6f9023051d8404e4b3ef81eb8808560
James <296466865@qq.com>
2019-05-07 14:33:23 +0000

    social authorizations

--- routes/api.php
+++ routes/api.php
@@ -43,4 +43,7 @@ $api->version('v1', [
     $api->post('captchas', 'CaptchasController@store')
          ->name('api.captchas.store');
 
+         // 第三方登录
+    $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+     ->name('api.socials.authorizations.store');
 });
